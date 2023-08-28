1f93d16fc138e1ee57ac877ba4f846b9d1215a61
WaiYanMoeMyintt <wymoemyint4523@gmail.com>
2023-08-28 23:49:13 +0630

    Add data files

---
+++ src/data/index.js
@@ -0,0 +1,215 @@
+import ghost from "../assets/ghost.png";
+import comedy from "../assets/comedy.png";
+import romance from "../assets/romance.png";
+import science from "../assets/science.png";
+import action from "../assets/action.png";
+import biography from "../assets/biography.png";
+
+export const films = [
+  {
+    id: 1,
+    name: action,
+    items: 2300,
+    title: "Action",
+  },
+  {
+    id: 2,
+    name: biography,
+    items: 1800,
+    title: "Biography",
+  },
+  {
+    id: 3,
+    name: ghost,
+    items: 3200,
+    title: "Horror",
+  },
+  {
+    id: 4,
+    name: romance,
+    items: 450,
+    title: "Romance",
+  },
+  {
+    id: 5,
+    name: science,
+    items: 1400,
+    title: "Science",
+  },
+  {
+    id: 6,
+    name: comedy,
+    items: 3000,
+    title: "Comedy",
+  },
+];
+
+import facebook from "../assets/facebook.svg";
+import instagram from "../assets/instagram.svg";
+import github from "../assets/github.svg";
+import linkedin from "../assets/linkedin.svg";
+import twitter from "../assets/twitter.svg";
+
+export const logos = [
+  {
+    id: 1,
+    name: facebook,
+    title: "facebook",
+  },
+  {
+    id: 2,
+    name: instagram,
+    title: "instagram",
+  },
+  {
+    id: 3,
+    name: twitter,
+    title: "Twitter",
+  },
+  {
+    id: 4,
+    name: github,
+    title: "github",
+  },
+  {
+    id: 1,
+    name: linkedin,
+    title: "linkedin",
+  },
+];
+
+export const banner = [
+  {
+    id: 1,
+    header: "Company",
+    name: [
+      {
+        id: 1,
+        title: "About us",
+      },
+      {
+        id: 2,
+        title: "Contact",
+      },
+      {
+        id: 3,
+        title: "Blog",
+      },
+    ],
+  },
+  {
+    id: 3,
+    header: "Partners",
+    name: [
+      {
+        id: 1,
+        title: "Advertise with us",
+      },
+      {
+        id: 2,
+        title: "Partner with US",
+      },
+      {
+        id: 3,
+        title: "Collabaration with US",
+      },
+    ],
+  },
+  {
+    id: 2,
+    header: "Support",
+    name: [
+      {
+        id: 1,
+        title: "Contact Support",
+      },
+      {
+        id: 2,
+        title: "Help Center",
+      },
+      {
+        id: 3,
+        title: "Supported Devices",
+      },
+      {
+        id: 4,
+        title: "Accessbility",
+      },
+      {
+        id: 5,
+        title: "Activate Your Device",
+      },
+    ],
+  },
+  {
+    id: 4,
+    header: "GET THE APPS",
+    name: [
+      {
+        id: 1,
+        title: "iOS",
+      },
+      {
+        id: 2,
+        title: "Android",
+      },
+      {
+        id: 3,
+        title: "Window",
+      },
+      {
+        id: 4,
+        title: "Mac",
+      },
+    ],
+  },
+  {
+    id: 5,
+    header: "PRESS",
+    name: [
+      {
+        id: 1,
+        title: "Press Releases",
+      },
+      {
+        id: 2,
+        title: "huru in News",
+      }
+    ],
+  },
+  {
+    id: 6,
+    header: "Legal",
+    name: [
+      {
+        id: 1,
+        title: "Privacy Policy (Updated)",
+      },
+      {
+        id: 2,
+        title: "Terms of Use (Updated)",
+      }
+    ],
+  },
+];
+
+import mircrosoft from "../assets/microsoft.svg";
+import apple from "../assets/apple.svg"
+import store  from "../assets/store.svg"
+export const download = [
+   {
+    id:1,
+    name:mircrosoft
+   },
+   {
+    id:2,
+    name:apple
+   },
+   
+   {
+    id:1,
+    name:store
+   },
+   
+
+]
