349be8f60fb563f3fc05ff1947c8c432debfed01
WaiYanMoeMyintt <wymoemyint4523@gmail.com>
2023-09-06 17:52:06 +0630

    Create multiple components and add necessary codes

---
+++ src/components/data/flim.js
@@ -0,0 +1,123 @@
+import action from "../../assets/film/action.jpg";
+import adventrues from "../../assets/film/adventures.jpg";
+import anime from "../../assets/film/anime.jpg";
+import cartoon from "../../assets/film/cartoon.jpg";
+import comedy from "../../assets/film/comedy.jpg";
+import documentary from "../../assets/film/documentary.jpg";
+import drama from "../../assets/film/drama.jpg";
+import family from "../../assets/film/family.jpg";
+import fantasy from "../../assets/film/fantasy.jpg";
+import halloween from "../../assets/film/halloween.jpg";
+import history from "../../assets/film/history.jpg";
+import horror from "../../assets/film/horror.jpg";
+import music from "../../assets/film/music.jpg";
+import mystery from "../../assets/film/mystery.jpg";
+import now from "../../assets/film/now.jpg";
+import romance from "../../assets/film/romance.jpg";
+import scifi from "../../assets/film/scifi.jpg";
+import tv from "../../assets/film/tv.jpg";
+import war from "../../assets/film/war.jpg";
+import western from "../../assets/film/western.jpg";
+
+
+export const explore = [
+     {
+        id:28,
+        name:"Action",
+        poster:action
+     }, {
+        id:12,
+        name:"Adventure",
+        poster:adventrues
+     },
+     {
+        id:16,
+        name:"Animation",
+        poster:anime
+     },
+     {
+        id:16,
+        name:"Cartoon",
+        poster:cartoon
+     },
+     {
+        id:35,
+        name:"Comedy",
+        poster:comedy
+     },
+     {
+        id:99,
+        name:"Documentary",
+        poster:documentary
+     },
+     {
+        id:18,
+        name:"Drama",
+        poster:drama
+     },
+     {
+        id:10751,
+        name:"Family",
+        poster:family
+     },
+     {
+        id:14,
+        name:"Fantasy",
+        poster:fantasy
+     },
+     {
+        id:53,
+        name:"Halloween",
+        poster:halloween
+     },
+     {
+        id:36,
+        name:"History",
+        poster:history
+     },
+     {
+        id:27,
+        name:"Horror",
+        poster:horror
+     },
+     {
+        id:10751,
+        name:"Now-Playing",
+        poster:now
+     },
+     {
+        id:9648,
+        name:"Mystery",
+        poster:mystery
+     },
+     {
+        id:10402,
+        name:"Music",
+        poster:music
+     },
+     {
+        id:10749,
+        name:"Romance",
+        poster:romance
+     },
+     {
+        id:878,
+        name:"SciFi",
+        poster:scifi
+     },
+     {
+        id:10770,
+        name:"TV-Shows",
+        poster:tv
+     },
+     {
+        id:10752,
+        name:"War",
+        poster:war
+     },
+     {
+        id:37,
+        name:"Western",
+        poster:western
+     }
+]
