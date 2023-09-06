a0f637be5cdc83baa0278b41ab0e3cb3c41d6864
WaiYanMoeMyintt <wymoemyint4523@gmail.com>
2023-09-06 21:25:28 +0630

    Create primary components and add some pieces of codes

---
+++ src/components/Streams.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import "./css/global.css";
+const Streams = () => {
+  const api =
+    `https://api.themoviedb.org/3/movie/now_playing?api_key=1b7c076a0e4849aeefd1f3c429c99a36&language=en-US&page=3`;
+  const imgUrl = "https://image.tmdb.org/t/p/original/";
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const movieCategories = async () => {
+      try {
+        const fetchData = await fetch(api);
+        const response = await fetchData.json();
+        setLoading(false);
+        setMovies(response.results);
+      } catch (err) {
+        return err.message;
+      }
+      finally{
+        setLoading(true)
+      }
+    };
+    movieCategories();
+  }, [movies]);
+  return (
+    <div className="movie_content_control relative">
+      <div className="movie_title">
+        <h2>Huru member's are now streaming</h2>
+      </div>
+
+      <div className="movies_content relative">
+        {movies.map((items) => (
+          <div>
+            {loading !== true ? (
+              <div className="loading_effect"></div>
+            ) : (
+              <Link to = {`/movies/${items.id}/${items.original_title}`} key={items.id}>
+                <img
+                  src={imgUrl + items.poster_path}
+                  alt={items.original_title}
+                />
+                <p>{items.original_title}</p>
+              </Link>
+            )}
+          </div>
+        ))}
+      </div>
+
+      <div className="banner w-100 flex justify-center items-center  text-center">
+        <p>End of the results.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Streams
